9f7669b01cf7ff98151a2ddcf403cbc4c79e70f7
vaasu <vaasubansal3100@gmail.com>
2026-09-01 16:48:48 +0530

    Force dynamic on all app pages

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
 import Link from "next/link";
--- app/(app)/section/[id]/page.tsx
+++ app/(app)/section/[id]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
 import { notFound, redirect } from "next/navigation";
