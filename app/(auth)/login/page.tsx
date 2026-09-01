ad26248469fe68ea20cfbb725fef2acc9f22b918
vaasu <vaasubansal3100@gmail.com>
2026-09-01 16:51:31 +0530

    Force dynamic on all pages permanently

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { signIn } from "@/lib/auth";
 import Link from "next/link";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Image from "next/image";
 
 export default function Home() {
